e396786bb1e5a403961b589271e77a908ea25144
chaudharykartik9 <tusharkartik8@gmail.com>
2026-07-26 13:16:08 +0530

    practice tailwind stying

--- tailwind-learning/src/App.jsx
+++ tailwind-learning/src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <h1 className="text-red-500 text-5xl font-bold">
-      Tailwind Test
-    </h1>
-  );
+    <>
+    <div className='grid grid-cols-10'>
+      <div className='bg-blue-300 sm:col-span-4 col-span-10  ' >child 1</div>
+      <div className='bg-red-300 sm:col-span-4 col-span-10'>child 2</div>
+      <div className='bg-green-200 sm:col-span-2 col-span-10' >child 3</div>
+    </div>
+    </>
+  )
 }
+
+export default App
